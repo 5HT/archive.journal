223de81f8d264b5a70b0d50e22f7833dea91e9cd
5HT <maxim@synrc.com>
2022-05-25 15:40:32 +0300

    simple XT disassembler in lovely nasm

---
+++ stream/2022/intel/XT/dasm.asm
@@ -0,0 +1,236 @@
+
+       global _main
+       default  rel
+       section .text
+
+%define syscall_exit    0x2000001
+%define syscall_write   0x2000004
+%define syscall_open    0x2000005
+%define syscall_close   0x2000006
+
+_main: mov rax, syscall_write ; greeting
+       mov rdi, 1
+       mov rsi, str
+       mov rdx, str.len
+       syscall
+       call show_pointer
+       mov rax, [machine.ptr]
+       inc qword [machine.ptr]
+       call parse_mod_rm
+       mov [inst0_1], rcx
+       mov [inst0_2], rbx
+       mov byte [inst0_3], 2
+       mov rcx, 3
+       mov rsi, inst0
+loop1: mov rax, [rsi+8]
+       push rsi
+       push rdi
+       call [rsi]
+       pop rdi
+       pop rsi
+       add rsi, 16
+       dec rcx
+       jnz loop1
+       call show_pointer
+       call print_mov
+       mov rax, [inst0_1]
+       call print_rm                ; [ ADDR
+       mov rax, 1
+       call print_hex_string
+       call closesq
+       call printcomma
+       mov rax, [inst0_2]
+       call print_reg               ; print REG
+       call println
+
+       call show_pointer
+
+       mov rax, 0x2000001 ; exit
+       xor rdi, rdi
+       syscall
+       ret
+
+closesq:
+       mov rsi, rmter
+       mov rdx, rmter.len
+       mov rax, syscall_write ; print ]
+       mov rdi, 1
+       syscall
+       ret
+
+printcomma:
+       mov rsi, comma
+       mov rdx, comma.len
+       mov rax, syscall_write ; print ,
+       mov rdi, 1
+       syscall
+       ret
+
+
+println:
+       mov rsi, linefeed
+       mov rdx, linefeed.len
+       mov rax, syscall_write ; print \n
+       mov rdi, 1
+       syscall
+       ret
+
+print_rm:
+       mov rdi, rax
+       shl rdi, 3
+       mov rbx, rm
+       mov rsi, [rbx+rdi]
+       mov rbx, rd
+       mov rdx, [rbx+rdi]
+       mov rax, syscall_write ; print addr
+       mov rdi, 1
+       syscall
+       ret
+
+show_pointer:
+       mov rsi, hex_emiter
+       mov rbx, hex
+       xor rax, rax
+       mov al, byte [machine.ptr]
+       and rax, 15
+       xlat
+       mov [rsi], al
+       mov rax, syscall_write ; print int
+       mov rsi, pointer
+       mov rdx, hex_emiter.len
+       mov rdi, 1
+       syscall
+       ret
+
+print_reg:
+       mov rsi, regb
+       shl rax, 1
+       add rsi, rax
+       mov rdx, 2
+       mov rax, syscall_write ; print register
+       mov rdi, 1
+       syscall
+       ret
+
+print_mov:
+       mov rsi, mov
+       mov rdx, mov.len
+       mov rax, syscall_write ; print MOV
+       mov rdi, 1
+       syscall
+       ret
+
+parse_mod_rm:
+       xor eax, eax
+       mov al, [machine.ptr] ; SI=src
+       xor ecx, ecx
+       mov cl, 3
+       mov ebx, eax
+       mov edx, eax
+       shr ebx, cl
+       add ecx, ecx
+       shr eax, cl   ; AX=mod
+       inc ecx
+       and ebx, ecx  ; BX=reg
+       and ecx, edx  ; CX=r/m
+       inc qword [machine.ptr]
+       ret
+
+print_hex_string:
+       xor rdx, rdx
+       push rax
+       mov rdx, rax
+       mov rdi, hex
+       mov rsi, machine
+       add rsi, [machine.ptr]
+       mov rbp, hexwout
+       mov rax, rdx
+       shl eax, 1
+       add eax, 2
+       inc eax
+       mov qword [hexwout.len], rax
+       dec eax
+       add rbp, rax
+loop:
+       mov bl, [rsi]
+       and ebx, 15
+       mov al, [rbx+rdi]
+       mov [rbp], al
+       dec rbp
+       mov bl, [rsi]
+       shr ebx, 4
+       and ebx, 15
+       mov al, [rbx+rdi]
+       mov [rbp], al
+       dec rbp
+       inc rsi
+       dec edx
+       jne loop
+
+       pop rdx
+       add qword [machine.ptr], rdx
+       mov rdx, qword [hexwout.len]
+
+       mov rax, syscall_write ; print hex
+       mov rsi, hexwout
+       mov rdi, 1
+       syscall
+       ret
+
+       section .data
+hexbout: db '+0xXX'
+.len: equ $-hexbout
+hexwout: db '+0xXXXXZZYYDDDFFFFFFFFFFFFF'
+.len: dq 4
+regb:  db "ALCLDLBLAHCHDHBH"
+regw:  db "AXCXDXBXSPBPSIDI"
+hex:   db '01234567890ABCDEF'
+rm000: db '[BX+SI'
+.len: equ $-rm000
+rm001: db '[BX+DI'
+.len: equ $-rm001
+rm010: db '[BP+SI'
+.len: equ $-rm010
+rm011: db '[BP+DI'
+.len: equ $-rm011
+rm100: db '[SI'
+.len: equ $-rm100
+rm101: db '[DI'
+.len: equ $-rm101
+rm110: db '[BP'
+.len: equ $-rm110
+rm111: db '[BX'
+.len: equ $-rm111
+rmter: db ']'
+.len: equ $-rmter
+comma: db ','
+.len: equ $-comma
+linefeed: db 10
+.len: equ $-linefeed
+inst0:    dq print_mov
+          dq 0
+          dq print_rm
+inst0_1:  dq 0
+          dq print_hex_string
+inst0_3:  dq 2
+          dq closesq
+          dq 0
+          dq printcomma
+          dq 0
+          dq print_reg
+inst0_2:  dq 0
+          dq println
+          dq 0
+inst0_p:  dq 0
+
+rm:    dq rm000,     rm001,     rm010,     rm011,     rm100,     rm101,     rm101,     rm110,     rm111
+rd:    dq rm000.len, rm001.len, rm010.len, rm011.len, rm100.len, rm101.len, rm101.len, rm110.len, rm111.len
+machine: db 0x00, 0b10111111, 0x13, 0x90, 0x91
+.ptr: dq 0
+mov: db 'MOV '
+.len: equ $-mov
+pointer:   db 'Pointer: '
+hex_emiter:  db 0, 10
+.len:  equ $-pointer
+str:   db "SYNRC DASM",10
+.len:  equ $-str
